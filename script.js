5d334b27724a1de841ccd74734cd43d9dc6d4d72
Samriddhi Shreyasee <112820579+Samiriddhi@users.noreply.github.com>
2023-09-01 23:24:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,50 +1,53 @@
 //your JS code here. If required.
- const tbody = document.querySelector('tbody');
-
-    let first = (Math.random()*3000).toPrecision(4);
-    let second = (Math.random()*3000).toPrecision(4);
-    let third = (Math.random()*3000).toPrecision(4);
-    
-    const prom1 = new Promise((resolve,reject)=>{
-            setTimeout(() => {
-                resolve('x');
-            }, first);
-        })
-        const prom2 = new Promise((resolve,reject)=>{
-            setTimeout(() => {
-                resolve('y');
-            }, second);
-        })
-        const prom3 = new Promise((resolve,reject)=>{
-            setTimeout(() => {
-                resolve('z');
-            }, third);
-        })
-
-        Promise.all([prom1, prom2, prom3]).then((values) => {
-            console.log(values);
-            makeTBody();
-        });
-        console.log(third);
-        // console.log(first.toPrecision(7), second, third);
-        let total = (Number(first)+Number(second)+Number(third)).toPrecision(4);
-        function makeTBody(){
-            let myStr = ` <tr>
-                <td>Promise 1</td>
-                <td>${first/1000}</td>
+const res = document.getElementById("output");
+ 
+const promises = [
+  new Promise((resolve) => {
+    const time = Math.floor(Math.random() * 3 + 1) * 1000;
+    setTimeout(() => resolve({ name: "Promise 1", time: time / 1000 }), time);
+  }),
+  new Promise((resolve) => {
+    const time = Math.floor(Math.random() * 3 + 1) * 1000;
+    setTimeout(() => resolve({ name: "Promise 2", time: time / 1000 }), time);
+  }),
+  new Promise((resolve) => {
+    const time = Math.floor(Math.random() * 3 + 1) * 1000;
+    setTimeout(() => resolve({ name: "Promise 3", time: time / 1000 }), time);
+  }),
+];
+ 
+async function callFns() {
+  const start = new Date();
+  // Use Promise.all to wait for all Promises to resolve
+  res.innerHTML += `
+            <tr id="loading">
+                <td colspan=2>Loading...</td>
             </tr>
+          `;
+  await Promise.all(promises)
+    .then((results) => {
+      res.innerHTML = ``;
+      // Log the array of results
+      results.forEach((e) => {
+        res.innerHTML += `
             <tr>
-                <td>Promise 2</td>
-                <td>${second/1000}</td>
+                <td>${e.name}</td>
+                <td>${e.time}</td>
             </tr>
-            <tr>
-                <td>Promise 3</td>
-                <td>${third/1000}</td>
-            </tr>
+          `;
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+ 
+  const end = new Date();
+ 
+  const timeInMillis = end - start;
+  res.innerHTML += `
             <tr>
                 <td>Total</td>
-                <td>${total/1000}</td>
-            </tr>`;
-            tbody.innerText='';
-            tbody.innerHTML = myStr;
-        }
+                <td>${timeInMillis / 1000}</td>
+            </tr>
+          `;
+}
